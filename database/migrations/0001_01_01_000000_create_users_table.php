b5de1aeca31d3e858707d3ed7d2b9651ee90e0a1
Agustya Pratama <agustyapratama123@gmail.com>
2025-05-29 13:34:43 +0700

    menambahkan model -a untuk mahasiswa, dosen, role, prodi

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role_id');
+            $table->boolean('is_active')->default(true);
+            $table->string('photo_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_05_28_130057_create_prodis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prodi', function (Blueprint $table) {
+            $table->id('id_prodi');
+            $table->string('kode_prodi')->unique();
+            $table->string('nama_prodi');
+            $table->string('fakultas');
+            $table->string('jenjang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodis');
+    }
+};
