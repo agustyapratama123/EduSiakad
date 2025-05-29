3b96d6b9662eb70f853417d37d804965493c5334
Agustya Pratama <agustyapratama123@gmail.com>
2025-05-29 20:44:19 +0700

    menambahkan model -a untuk mata_kuliah

---
+++ app/Models/MataKuliah.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MataKuliah extends Model
+{
+    /** @use HasFactory<\Database\Factories\MataKuliahFactory> */
+    use HasFactory;
+
+    protected $table = 'mata_kuliah';
+
+    protected $fillable = [
+        'kode',
+        'nama',
+        'sks',
+        'semester',
+        'deskripsi',
+    ];
+
+}
